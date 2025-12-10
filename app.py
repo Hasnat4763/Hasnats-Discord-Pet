1e4bf50a459623dc16b453cff913dbdb35bd38c9
Hasnat4763 <abulhasnat5737@gmail.com>
2025-12-10 20:47:09 +0600

    Lol

---
+++ app.py
@@ -0,0 +1,38 @@
+import discord
+from discord.ext import commands
+import dotenv
+import os
+dotenv.load_dotenv(".env")
+
+TOKEN=os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="/67", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if "chirlie" in message.content.lower() or "kirk" in message.content.lower():
+        await message.channel.send("WE ARE CHIRLIE KIRK")
+    
+    if "67" in message.content:
+        await message.channel.send("SIX SEVEN")
+    
+    if "hasina" in message.content.lower():
+        await message.channel.send("Did you mean Sanigga?")
+    
+    
+    if "brat" in message.content.lower():
+        await message.channel.send("Brat aint Sigma")
+        
+    if "hasnat" in message.content.lower():
+        await message.channel.send("Hasnat is the real Sigma")
+    
+if TOKEN:
+    bot.run(token=TOKEN)
